9134504e76613f5183de7aba25ba3fc494ae5ac5
caleb-freitas <calebfreitas@tutanota.com>
2022-05-10 17:13:48 -0300

    chore: add server layout

--- src/schema.ts
+++ src/schema.ts
@@ -4,7 +4,15 @@ import { join } from 'path'
 export const schema = makeSchema({
 	types: [],
 	outputs: {
+		/**
+		 * schema.graphql::is the graphql schema definition language for defining the structure of the api
+		 */
 		schema: join(process.cwd(), "schema.graphql"),
+		/**
+		 * nexus-typegen.ts::contain typescript type definitions for all types in graphql schema
+		 */
 		typegen: join(process.cwd(), "nexus-typegen.ts")
 	}
 })
+
+console.log(process.cwd())
