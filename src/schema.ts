d6dd6ac9c3c1091acb9126c7ae4a72fbef6796d0
caleb-freitas <calebfreitas@tutanota.com>
2022-05-11 11:51:23 -0300

    feat: add feed query

--- src/schema.ts
+++ src/schema.ts
@@ -1,16 +1,13 @@
 import { makeSchema } from 'nexus'
 import { join } from 'path'
+import * as types from "./graphql"
 
 export const schema = makeSchema({
-	types: [],
+	types,
 	outputs: {
-		/**
-		 * schema.graphql::is the graphql schema definition language for defining the structure of the api
-		 */
+		// schema.graphql::is the graphql schema definition language for defining the structure of the api
 		schema: join(process.cwd(), "schema.graphql"),
-		/**
-		 * nexus-typegen.ts::contain typescript type definitions for all types in graphql schema
-		 */
+		// nexus-typegen.ts::contain typescript type definitions for all types in graphql schema
 		typegen: join(process.cwd(), "nexus-typegen.ts")
 	}
 })
