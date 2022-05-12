ee22c6afe2bcd7b1708196039653a9eb64c4bafe
caleb-freitas <calebfreitas@tutanota.com>
2022-05-12 14:52:22 -0300

    chore: add deployment setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { ApolloServer } from "apollo-server"
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core"
 import { schema } from "./schema"
 import { context } from "./context"
 
 export const server = new ApolloServer({
   schema,
-  context
+  context,
+  introspection: true,                                      // 1
+  plugins: [ApolloServerPluginLandingPageLocalDefault()]
 })
 
-const port = 4000
+const port = process.env.PORT || 3000
 
 server.listen({ port }).then(({ url }) => {
   console.log(`Server running at ${url}`)
